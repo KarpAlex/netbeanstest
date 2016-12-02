f3ea993fb9f37780eb30c5d10116a704d081fed5
akarpenko <akarpenko@akarpenko-HP>
2016-12-02 12:05:05 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "ich experimentiere auch hier. lg ioulia";
+        echo "ich experimentiere auch hier. lg Alexei";
         ?>
     </body>
 </html>

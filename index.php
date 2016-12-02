dec2f9bbe56010de4fc5c4c61b38e3ed7fda65b9
I-Kon <wd15k003@technikum-wien.at>
2016-12-02 11:58:55 +0100

    hello from ioulia 1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo ich experimentiere hier;
+        echo "ich experimentiere auch hier. lg ioulia";
         ?>
     </body>
 </html>

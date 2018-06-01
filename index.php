0a63c75c5a11ab2930c366909d02487f6bce0a27
alex <karpen_ko@hotmail.com>
2018-06-01 10:23:05 +0200

    This is second commit

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ and open the template in the editor.
         // put your code here
         echo "JAWOHHHHHHHHHHHHHHHHL";
 	echo "Das ist erster Commit";
+	echo "Das ist zweiter Commit";
         ?>
     </body>
 </html>

0038c2bc2ec07b3270f7db8844dcb137e69b370c
alex <karpen_ko@hotmail.com>
2018-06-01 10:20:43 +0200

    first commit

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ and open the template in the editor.
         <?php
         // put your code here
         echo "JAWOHHHHHHHHHHHHHHHHL";
+	echo "Das ist erster Commit";
         ?>
     </body>
 </html>
